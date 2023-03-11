76b6f12557caf44f9c90f8f1500c42a93eb3db52
Tazim381 <jucse28.381@gmail.com>
2023-03-11 11:30:22 +0600

    class counter added

---
+++ reactcounterusingclassbasedcomponent/src/counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <div>
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Counter;
